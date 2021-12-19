19feb2623bdd3c421224ce3624fff2de9f004ba5
DennisKlpng <dennis.kloepping@web.de>
2021-12-19 22:12:15 +0100

    Pt2 solution. Using functools instead of numpy prod, since the latter apparently yields imprecise results

--- AOC_16.py
+++ AOC_16.py
@@ -1,5 +1,5 @@
 import sys
-import numpy as np
+from functools import reduce
 
 
 conversion = {'0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100', '5': '0101', '6': '0110',
@@ -15,8 +15,7 @@ if __name__ == '__main__':
             input_code = input_code.strip()
             for i in range(len(input_code)):
                 binary_input_code += conversion[input_code[i]]
-            binary_input_code = binary_input_code.rstrip("0")
-            print("Start parsing " + input_code)
+            binary_input_code = binary_input_code
 
             def parse_package(start, version_sum):
                 # get version
@@ -25,13 +24,15 @@ if __name__ == '__main__':
                 start += 3
                 pkg_type = int(binary_input_code[start:start + 3], 2)
                 start += 3
+                val = -1
                 if 4 == pkg_type:
-                    start = parse_literal_package(start)
+                    start, val = parse_literal_package(start)
                 else:
                     ret = parse_operator_package(start, version_sum, pkg_type)
                     start = ret[0]
                     version_sum = ret[1]
-                return start, version_sum
+                    val = ret[2]
+                return start, version_sum, val
 
             def parse_literal_package(start):
                 num = ""
@@ -41,13 +42,13 @@ if __name__ == '__main__':
                     start += 5
                     if "0" == cont_bit:
                         break
-                print("Literal pkg contained number " + str(int(num,2)))
-                return start
+                return start, int(num, 2)
 
             def parse_operator_package(start, version_sum, type):
                 length_type = int(binary_input_code[start], 2)
                 start += 1
-                print("Operator package with type " + str(length_type))
+                values = []
+                result = 0
                 if 0 == length_type:
                     len_pkg = int(binary_input_code[start:start + 15], 2)
                     start += 15
@@ -57,6 +58,7 @@ if __name__ == '__main__':
                         ret = parse_package(start, version_sum)
                         start = ret[0]
                         version_sum = ret[1]
+                        values.append(ret[2])
                 else:
                     num_pkg = int(binary_input_code[start:start + 11], 2)
                     start += 11
@@ -65,7 +67,24 @@ if __name__ == '__main__':
                         ret = parse_package(start, version_sum)
                         start = ret[0]
                         version_sum = ret[1]
-                return start, version_sum
+                        values.append(ret[2])
+                if 0 == type:
+                    result = sum(values)
+                elif 1 == type:
+                    result = reduce((lambda x, y: x*y), values)
+                elif 2 == type:
+                    result = min(values)
+                elif 3 == type:
+                    result = max(values)
+                elif 5 == type:
+                    result = 1 if values[0] > values[1] else 0
+                elif 6 == type:
+                    result = 1 if values[0] < values[1] else 0
+                elif 7 == type:
+                    result = 1 if values[0] == values[1] else 0
+                if result == 1291129408:
+                    print(values)
+                return start, version_sum, result
 
             start, version_sum_all = 0, 0
             ret = parse_package(start, version_sum_all)
@@ -73,3 +92,4 @@ if __name__ == '__main__':
             version_sum_all = ret[1]
 
             print("Complete version: " + str(version_sum_all))
+            print("Return val: " + str(ret[2]))
