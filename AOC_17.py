f7da142b49d917a1793be9c5cc0e0f2aa9c5cf0f
DennisKlpng <dennis.kloepping@web.de>
2021-12-20 19:23:25 +0100

    Mostly mathematical solution for pt2, brute force within the ranges

--- AOC_17.py
+++ AOC_17.py
@@ -1,5 +1,15 @@
 import sys
 import re
+import math
+
+
+def calc_pos_vel(init_vel, t, is_x):
+    if is_x:
+        if t > init_vel:  # account for x_vel never being < 0
+            return 0.5*(init_vel * init_vel + init_vel)
+        return t * init_vel - 0.5*(t * t - t)
+    else:
+        return t * init_vel - 0.5*(t * t - t)
 
 
 if __name__ == '__main__':
@@ -23,4 +33,34 @@ if __name__ == '__main__':
 
         y_max = 0.5 * (pow(y_vel_opt, 2) + y_vel_opt)
         print("Solution pt 1: " + str(y_max))
+
+        # pt 2
+        # for y: any solution with y_vel > -y_tgt_min - 1 is impossible
+        # any solution with y_vel < -tgt_min is also impossible, since this will just directly overshoot
+        y_max = -yvals[0] - 1
+        y_min = yvals[0]
+
+        # for x: we actually need to reach the tgt, which means, that x(t) >= x_tgt_min and no overshoot
+        x_max = xvals[1]  # trivial
+        # since x_vel_(t) = x_vel - t, we can just calc the x pos from gauss as before
+        # (x_pos(t) = t * x_vel - sum(0..t)(t-1) = t*x_vel - (t^2 - t)/2 (as long as t > 0)
+        # the maximum is the same as above, at x_pos_max = (x_vel^2 + x_vel)/2, inverting that yields:
+        x_min = int(math.sqrt(2 * xvals[0] + 0.25) - 0.5) + 1
+
+        # Now brute force the solution
+        num_valid_solutions = 0
+        for x in range(x_min, x_max+1):
+            for y in range(y_min, y_max+1):
+                step = 0
+                while True:
+                    step += 1
+                    x_pos = calc_pos_vel(x, step, True)
+                    y_pos = calc_pos_vel(y, step, False)
+                    if xvals[0] <= x_pos <= xvals[1] and yvals[0] <= y_pos <= yvals[1]:
+                        num_valid_solutions += 1
+                        break
+                    if x_pos > xvals[1] or y_pos < yvals[0]:
+                        break  # will not hit
+
+        print("Solution pt 2: " + str(num_valid_solutions))
 
