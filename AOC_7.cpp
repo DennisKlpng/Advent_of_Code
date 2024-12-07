bf2bff60e12e3124be7afcb4e0936e1103e8a303
DennisKlpng <dennis.kloepping@web.de>
2024-12-07 11:59:36 +0100

    Some performance improvement by not adapting the deque

--- AOC_7.cpp
+++ AOC_7.cpp
@@ -1,22 +1,27 @@
 #include "utils.hpp"
 #include <deque>
 #include <functional>
+#include <cmath>
 
 inline uint64_t add(uint64_t a, uint64_t b){return a + b;};
 inline uint64_t mul(uint64_t a, uint64_t b){return a * b;};
-inline uint64_t concat(uint64_t a, uint64_t b){return stoll(std::to_string(a) + std::to_string(b));}
+inline uint64_t concat(uint64_t a, uint64_t b){
+    uint64_t val = b;
+    while(b != 0) {b /= 10; a*= 10;}
+    return val + a;
+}
 
 static const std::vector<std::function<uint64_t(uint64_t, uint64_t)>> funcs_pt1{add, mul};
 static const std::vector<std::function<uint64_t(uint64_t, uint64_t)>> funcs_pt2{add, mul, concat};
 
-bool check_if_valid(uint64_t tgt_result, std::deque<uint64_t> input,
-                    std::vector<std::function<uint64_t(uint64_t, uint64_t)>> ops){
-    if(input.size() == 1) return tgt_result == input[0];
+bool check_if_valid(uint64_t tgt_result, const std::deque<uint64_t>& input,
+                    std::vector<std::function<uint64_t(uint64_t, uint64_t)>> ops,
+                    uint64_t curr_value, uint8_t index){
+    if(input.size() == index) return tgt_result == curr_value;
     for(auto& func: ops){
-        std::deque<uint64_t> in_loc = input;
-        in_loc[1] = func(in_loc[0], in_loc[1]);
-        in_loc.pop_front();
-        if(check_if_valid(tgt_result, in_loc, ops)) return true;
+        uint64_t new_value = func(curr_value, input[index]);
+        uint8_t new_index = index + 1;
+        if(check_if_valid(tgt_result, input, ops, new_value, new_index)) return true;
     }
 
     return false;
@@ -30,8 +35,10 @@ std::pair<uint64_t, uint64_t> solve_puzzle(std::string filename){
         std::deque<uint64_t> data = split_string_int<std::deque<uint64_t>>(line, ' ', ":");
         uint64_t tgt = data[0];
         data.pop_front();
-        if(check_if_valid(tgt, data, funcs_pt1)) res.first += tgt;
-        if(check_if_valid(tgt, data, funcs_pt2)) res.second += tgt;
+        uint64_t init_val = data[0];
+        data.pop_front();
+        if(check_if_valid(tgt, data, funcs_pt1, init_val, 0)) res.first += tgt;
+        if(check_if_valid(tgt, data, funcs_pt2, init_val, 0)) res.second += tgt;
     }
 
     return res;
