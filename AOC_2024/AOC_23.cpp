8b7c6e6a72aa3f8e25deec80e535eaa405ee6824
DennisKlpng <dennis.kloepping@web.de>
2024-12-23 15:00:48 +0100

    Solving part 2 with Bron-Kerbosch algorithm

--- AOC_2024/AOC_23.cpp
+++ AOC_2024/AOC_23.cpp
@@ -2,9 +2,31 @@
 
 typedef std::map<std::string, std::vector<std::string>> graph_map;
 
+typedef std::set<std::string> strset;
+void BronKerbosch(const graph_map& graph, strset& R, strset& P, strset& X, strset& maxR){
+    if(P.empty() && X.empty()) {
+        if(R.size() > maxR.size()){
+            maxR = R;
+        }
+        return;
+    }
+    auto copy_P = P;
+    for(auto& v: copy_P){
+        strset newP, newX;
+        auto newR = R;
+        newR.emplace(v);
+        for(auto& nb: graph.at(v)){
+            if(P.contains(nb)) newP.emplace(nb);
+            if(X.contains(nb)) newX.emplace(nb);
+        }
+        BronKerbosch(graph, newR, newP, newX, maxR);
+        P.erase(v);
+        X.emplace(v);
+    }
+}
 
-std::pair<uint64_t, uint64_t> solve_puzzle(std::string filename){
-    std::pair<uint64_t, uint64_t> res{0, 0};
+std::pair<uint64_t, std::string> solve_puzzle(std::string filename){
+    std::pair<uint64_t, std::string> res{0, ""};
 
     auto data = read_file_as_lines(filename);
 
@@ -39,11 +61,16 @@ std::pair<uint64_t, uint64_t> solve_puzzle(std::string filename){
         if(std::any_of(tr.begin(), tr.end(),[](std::string i){return i[0] == 't';})) res.first++;
     }
     res.first/=3;
+    strset R{}, X{}, maxR{};
+    BronKerbosch(graph, R, allnodes, X, maxR);
+    for(auto& s: maxR){
+        res.second += s + ",";
+    }
     return res;
 }
 
 int main(){
-    std::pair<uint64_t, uint64_t> res = solve_puzzle("inputs/Test_23.txt");
+    std::pair<uint64_t, std::string> res = solve_puzzle("inputs/Test_23.txt");
     print("Test res pt 1:", res.first, "pt 2:", res.second);
     double time_spent;
     res = profile_function(solve_puzzle, time_spent, "inputs/Data_23.txt");
