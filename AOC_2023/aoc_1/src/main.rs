8b2a3f60eee0d78918649c6fc3b7e2af0f353c74
DennisKlpng <dennis.kloepping@web.de>
2024-12-29 23:23:17 +0100

    Regex solution for pt 1

--- AOC_2023/aoc_1/src/main.rs
+++ AOC_2023/aoc_1/src/main.rs
@@ -1,10 +1,17 @@
 use rust_utils as utils;
+use regex::Regex;
 
 fn solve(filename: &str) -> (u64, u64){
-    let res = (0u64, 0u64);
+    let mut res = (0u64, 0u64);
     if let Ok(lines) = utils::file_read::read_file_as_lines(filename){
         for line in lines.flatten() {
             // println!("{}", line);
+            let re = Regex::new(r"[0-9]").unwrap();
+            let matches:Vec<&str> = re.find_iter(&line).map(|x| x.as_str()).collect();
+            // concat strings into common string and convert to int (unwrap since first/last returns optional)
+            let val:u64 = format!("{}{}", matches.first().unwrap(), matches.last().unwrap()).parse().unwrap();
+            // println!("{}", val);
+            res.0 += val;
         }
     };
 
