2c34f930bbab589152f1ba4574ecf8981d7b7891
DennisKlpng <dennis.kloepping@web.de>
2025-01-06 00:49:36 +0100

    Todo: part 2, dealing with every single seed is not going to work Gotta have to deal with ranges consistently, which won't be pretty

--- AOC_2023/aoc_5/src/main.rs
+++ AOC_2023/aoc_5/src/main.rs
@@ -15,11 +15,25 @@ fn fold_level(lvl_mapping: &Vec<Mapping>, num: i64) -> i64{
     num
 }
 
+fn get_min_pos(seeds: &Vec<i64>, mappings: &Vec<Vec<Mapping>>) -> u64{
+    let mut pos: Vec<i64> = vec![];
+    for seed in seeds{
+        //iter over vector of mappings
+        //in each step we fold a mapping from the almanach and use the new value for next step:
+        //seed => (transform with seed2soil mapping) => soil => ... => position
+        pos.push(mappings.iter()
+            .fold(*seed, |acc, x| fold_level(x, acc)));
+    }
+    *pos.iter().min().unwrap() as u64
+}
+
 fn solve(filename: &str) -> (u64, u64){
     let mut res = (0u64, 0u64);
     let mut mappings: Vec<Vec<Mapping>> = vec![];
     if let Ok(blocks) = utils::file_read::read_file_block_lines(filename){
+        //the first block is the list of seeds
         let seeds = utils::string_utils::split_string_i64(blocks.first().unwrap().first().unwrap());
+        //every block after the first is a mapping
         for block in blocks.iter().skip(1) {
             let mut type_mapping: Vec<Mapping> = vec![];
             for line in block{
@@ -33,12 +47,15 @@ fn solve(filename: &str) -> (u64, u64){
             }
             mappings.push(type_mapping);
         }
-        let mut pos: Vec<i64> = vec![];
-        for seed in seeds{
-            pos.push(mappings.iter()
-               .fold(seed, |acc, x| fold_level(&x, acc)));
+        res.0 = get_min_pos(&seeds, &mappings);
+        //extend seeds by ranges: zip iter getting all starts (index 0, 2, ..) with iter getting all ends (1, 3, ..)
+        //basically we create a loop here that uses i and i+1 in every step and iterates i by 2
+        //then we append the whole range start..end=start+length (exclusive) to the new seeds list
+        let mut seeds_ext: Vec<i64> = vec![];
+        for (a, b) in seeds.iter().step_by(2).zip(seeds.iter().skip(1).step_by(2)){
+            seeds_ext.append(&mut (*a..(*a + *b)).collect::<Vec<i64>>());
         }
-        res.0 = *pos.iter().min().unwrap() as u64;
+        res.1 = get_min_pos(&seeds_ext, &mappings);
     };
 
     res
