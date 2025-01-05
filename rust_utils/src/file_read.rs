b884637fd3657c373a1ac76f8260cd5507712117
DennisKlpng <dennis.kloepping@web.de>
2025-01-05 23:43:28 +0100

    Added option to split file input into blocks (by empty lines) String splitting into ints now works with non-int-substrings (less clean though)

--- rust_utils/src/file_read.rs
+++ rust_utils/src/file_read.rs
@@ -7,3 +7,26 @@ where T: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines().flatten().collect::<Vec<String>>())
 }
+
+pub fn read_file_block_lines<T>(filename: T) -> io::Result<Vec<Vec<String>>>
+where T: AsRef<Path>, {
+    let mut res: Vec<Vec<String>> = vec![];
+    let mut block_lines : Vec<String> = vec![];
+    if let Ok(lines) = read_file_as_lines(filename){
+        for line in lines{
+            if line.is_empty(){
+                res.push(block_lines.clone());
+                block_lines.clear();
+            }
+            else{
+                block_lines.push(line);
+            }
+        }
+        //eof
+        res.push(block_lines.clone());
+        Ok(res)
+    }
+    else{
+        panic!("Unable to parse input into blocks")
+    }
+}
--- rust_utils/src/string_utils.rs
+++ rust_utils/src/string_utils.rs
@@ -1,11 +1,29 @@
 pub fn split_string_i64(parse_str: &str) -> Vec<i64>{
-    return parse_str.split_whitespace().map(|val| val.parse().unwrap()).collect::<Vec<i64>>();
+    let mut res: Vec<i64> = vec![];
+    for val in parse_str.split_whitespace(){
+        if let Ok(x) = val.parse::<i64>(){
+            res.push(x);
+        }
+    }
+    res
 }
 
 pub fn split_string_u64(parse_str: &str) -> Vec<u64>{
-    return parse_str.split_whitespace().map(|val| val.parse().unwrap()).collect::<Vec<u64>>();
+    let mut res: Vec<u64> = vec![];
+    for val in parse_str.split_whitespace(){
+        if let Ok(x) = val.parse::<u64>(){
+            res.push(x);
+        }
+    }
+    res
 }
 
 pub fn split_string_f64(parse_str: &str) -> Vec<f64>{
-    return parse_str.split_whitespace().map(|val| val.parse().unwrap()).collect::<Vec<f64>>();
+    let mut res: Vec<f64> = vec![];
+    for val in parse_str.split_whitespace(){
+        if let Ok(x) = val.parse::<f64>(){
+            res.push(x);
+        }
+    }
+    res
 }
