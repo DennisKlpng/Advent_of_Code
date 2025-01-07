c438f966a1c0d5e5bc0eaf638813de0dcede0f18
DennisKlpng <dennis.kloepping@web.de>
2025-01-08 00:52:46 +0100

    Part 2 done by messing with card counts and replacing jokers in input string

--- AOC_2023/aoc_7/src/main.rs
+++ AOC_2023/aoc_7/src/main.rs
@@ -2,7 +2,7 @@ use std::{cmp::Ordering, collections::HashMap};
 
 use rust_utils as utils;
 
-static CARD_VALS : [char; 13] = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+static CARD_VALS : [char; 14] = ['0', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
 
 #[derive(Eq, PartialEq)]
 struct Hand{
@@ -12,12 +12,29 @@ struct Hand{
 }
 
 impl Hand{
-    fn calc_hand_type(&mut self){
+    fn calc_hand_type(&mut self, pt2: bool){
         //count number of occurences
-        let counts = self.cards.chars().fold(HashMap::new(), |mut acc, card| {
+        let mut counts = self.cards.chars().fold(HashMap::new(), |mut acc, card| {
             *acc.entry(card).or_insert(0) += 1;
             acc
         });
+
+        //consider jokers
+        if pt2{
+            //first remove them from map of counts
+            let num_jokers = counts.remove(&'J').unwrap_or_default();
+
+            //improve hand with jokers => it's always most beneficial to add them to the max count
+            //if there are no counts in there (all jokers in input): return and insert some arbitrary key
+            //key isn't used afterwards anyways, only the counts
+            let key = counts.iter().max_by_key(|x| x.1).unwrap_or((&'X', &0)).0;
+            *counts.entry(*key).or_insert(0) += num_jokers;
+
+            //replace jokers in cards with '0's for new tie-breaking
+            self.cards = self.cards.replace("J", "0");
+        }
+
+
         let mut counts = counts.values().into_iter().collect::<Vec<_>>();
         counts.sort_unstable();
         self.hand_type = match counts.as_slice(){
@@ -66,18 +83,30 @@ impl PartialOrd for Hand{
     }
 }
 
+fn calc_score(hands: &mut Vec<Hand>, pt2: bool) -> u64{
+    let mut res = 0;
+    if pt2{
+        for h in &mut *hands{
+            //recalc hand types
+            h.calc_hand_type(pt2);
+        }
+    }
+    hands.sort();
+    res = hands.iter().zip(1u32..).fold(res, |acc, (hand, rank)| acc + (hand.bid*rank) as u64);
+    res
+}
+
 fn solve(filename: &str) -> (u64, u64){
     let mut res: (u64, u64) = (0u64, 0u64);
     if let Ok(lines) = utils::file_read::read_file_as_lines(filename){
         let mut hands = vec![];
         for line in lines {
-            //lexographical sort cards => we can just use the default Eq and PartialEq implementations
             let mut hand = Hand{cards: line[0..5].to_owned(), hand_type: 0, bid: line[6..].parse::<u32>().unwrap()};
-            hand.calc_hand_type();
+            hand.calc_hand_type(false);
             hands.push(hand);
         }
-        hands.sort();
-        res.0 = hands.iter().zip(1u32..).fold(res.0, |acc, (hand, rank)| acc + (hand.bid*rank) as u64);
+        
+        (res.0, res.1) = (calc_score(&mut hands, false), calc_score(&mut hands, true));
     };
 
     res
